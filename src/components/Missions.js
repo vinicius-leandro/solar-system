a2a2e13838098cc3cfa1adb17eb0421862f9ceb1
vinicius leandro <leandrocostavinicius@gmail.com>
2022-06-21 20:11:26 -0300

    requisito 7

---
+++ src/components/Missions.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <div data-testid="missions">a</div>
+    );
+  }
+}
+
+export default Missions;
